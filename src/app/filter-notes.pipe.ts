73c5bf35b9facaa5e261ce79c4f2ee0e225e6f9a
Omar Brown <33111209+optimaldev@users.noreply.github.com>
2022-07-07 09:20:45 -0400

    Ran formatter on pipe

--- src/app/filter-notes.pipe.ts
+++ src/app/filter-notes.pipe.ts
@@ -1,12 +1,10 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
-  name: 'filterNotes'
+  name: 'filterNotes',
 })
 export class FilterNotesPipe implements PipeTransform {
-
   transform(value: string | number): string {
-    return value ? value.toString() : "N/A";
+    return value ? value.toString() : 'N/A';
   }
-
 }
